5bf9451d4e1d1e3cc8178c4ae289bb60b81203b6
Anush132 <116074251+Anush132@users.noreply.github.com>
2022-12-23 00:34:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,172 +40,233 @@ def College():
 
 @app.route('/login', methods =['GET', 'POST'])
 def login():
-	global id
-	msg = ''
-	if request.method == 'POST' and 'college' in request.form and 'id' in request.form and 'username' in request.form and 'password' in request.form:
-		college = request.form['college']
-		id = request.form['id']
-		username = request.form['username']
-		password = request.form['password']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE college = % s AND username = % s AND password = % s', (college, username, password, ))
-		account = cursor.fetchone()
-		if account:
-			session['loggedin'] = True
-			session['id'] = account['id']
-			session['username'] = account['username']
-			msg = 'Logged in successfully !'
-			# cur.execute('INSERT into eventss VALUES (%s,)',(id,))
-			# # data = cur.fetchall()
-			# cur.close()
-			insert_stmt = ("INSERT INTO eventss (id) VALUES (%s)")
-			data = (id)
-			cursor.execute(insert_stmt, data)
-			mysql.connection.commit()
-			return render_template('index2.html',eventss=data)
-		elif not college or not id or not username or not password :
-			msg = 'Details missing!'
-		else:
-			msg = 'Incorrect username / password !'
-	return render_template('login.html', msg = msg)
+    global id
+    msg = ''
+    if request.method == 'POST' and 'college' in request.form and 'id' in request.form and 'username' in request.form and 'password' in request.form:
+        college = request.form['college']
+        id = request.form['id']
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE college = % s AND username = % s AND password = % s', (college, username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            # cur.execute('INSERT into eventss VALUES (%s,)',(id,))
+            # # data = cur.fetchall()
+            # cur.close()
+            # insert_stmt = ("INSERT INTO eventss (id) VALUES (%s)")
+            # data = (id)
+            # cursor.execute(insert_stmt, data)
+            # mysql.connection.commit()
+            return render_template('index2.html',msg=msg)
+        elif not college or not id or not username or not password :
+            msg = 'Details missing!'
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
 
 @app.route('/logout')
 def logout():
-	session.pop('loggedin', None)
-	session.pop('id', None)
-	session.pop('username', None)
-	return redirect(url_for('login'))
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 @app.route('/register', methods =['GET', 'POST'])
 def register():
-	msg = ''
-	if request.method == 'POST' and 'college' in request.form and 'id' in request.form and 'username' in request.form and 'password' in request.form and 'email' in request.form :
-		college = request.form['college']
-		id = request.form['id']
-		username = request.form['username']
-		password = request.form['password']
-		email = request.form['email']
-		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-		cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
-		account = cursor.fetchone()
-		if account:
-			msg = 'Account already exists !'
-		elif not college or not id or not username or not password or not email:
-			msg = 'Please fill out the form !'
-		elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-			msg = 'Invalid email address !'
-		elif not re.match(r'[A-Za-z0-9]+', username):
-			msg = 'Username must contain only characters and numbers !'
-		else:
-			cursor.execute('INSERT INTO accounts VALUES (%s, % s, % s, % s, % s)', (college, id, username, password, email, ))
-			mysql.connection.commit()
-			msg = 'You have successfully registered !'
-			return render_template('login.html', msg = msg)
-	elif request.method == 'POST':
-		msg = 'Please fill out the form !'
-	return render_template('register.html', msg = msg)
+    msg = ''
+    if request.method == 'POST' and 'college' in request.form and 'id' in request.form and 'username' in request.form and 'password' in request.form and 'email' in request.form :
+        college = request.form['college']
+        id = request.form['id']
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not college or not id or not username or not password or not email:
+            msg = 'Please fill out the form !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (%s, % s, % s, % s, % s)', (college, id, username, password, email, ))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+            return render_template('login.html', msg = msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
+
+@app.route('/stulogin', methods =['GET', 'POST'])
+def stulogin():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM students WHERE username = % s AND password = % s', ( username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            # session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('studSecond.html')
+        elif not username or not password :
+            msg = 'Details missing!'
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('stulogin.html', msg = msg)
+
+@app.route('/sturegister', methods =['GET', 'POST'])
+def sturegister():
+    msg = ''
+    if request.method == 'POST' and 'first_name' in request.form and 'last_name' in request.form and 'phone' in request.form and 'email' in request.form and 'college' in request.form and 'username' in request.form and 'password' in request.form  :
+        first_name = request.form['first_name']
+        last_name= request.form['last_name']
+        phone = request.form['phone']
+        email = request.form['email']
+        college = request.form['college']
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM students WHERE username = % s', (username, ))
+        student = cursor.fetchone()
+        if student:
+            msg = 'Account already exists !'
+        elif not college or not first_name or not last_name or not phone or not username or not password or not email:
+            msg = 'Please fill out the form !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'\d{10}', phone):
+            msg = 'Invalid phone number !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        else:
+            cursor.execute('INSERT INTO students VALUES (%s, % s, % s, % s, % s, % s, %s)', (first_name, last_name,phone,email,college, username, password))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+            return render_template('stulogin.html', msg = msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('sturegister.html', msg = msg)
+
 
 @app.route('/Hackathon')
 def Hackathon():	
-	cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-	cursor.execute('SELECT * FROM eventss where event_type="Hackathon"')
-	result = cursor.fetchall()
-	cursor.close()
-	return render_template('studSecond.html',eventss=result)
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM eventss where event_type="Hackathon"')
+    result = cursor.fetchall()
+    cursor.close()
+    return render_template('studSecond.html',eventss=result)
 
 @app.route('/Workshop')
 def Workshop():	
-	cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-	cursor.execute('SELECT * FROM eventss where event_type="Workshop"')
-	result = cursor.fetchall()
-	cursor.close()
-	return render_template('studSecond.html',eventss=result)
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM eventss where event_type="Workshop"')
+    result = cursor.fetchall()
+    cursor.close()
+    return render_template('studSecond.html',eventss=result)
 
 @app.route('/Sports')
 def Sports():	
-	cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-	cursor.execute('SELECT * FROM eventss where event_type="Sports"')
-	result = cursor.fetchall()
-	cursor.close()
-	return render_template('studSecond.html',eventss=result)
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM eventss where event_type="Sports"')
+    result = cursor.fetchall()
+    cursor.close()
+    return render_template('studSecond.html',eventss=result)
 
 @app.route('/Cultural')
 def Cultural():	
-	cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-	cursor.execute('SELECT * FROM eventss where event_type="Cultural"')
-	result = cursor.fetchall()
-	cursor.close()
-	return render_template('studSecond.html',eventss=result)
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM eventss where event_type="Cultural"')
+    result = cursor.fetchall()
+    cursor.close()
+    return render_template('studSecond.html',eventss=result)
 
 @app.route('/Seminar')
 def Seminar():	
-	cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-	cursor.execute('SELECT * FROM eventss where event_type="Seminar"')
-	result = cursor.fetchall()
-	cursor.close()
-	return render_template('studSecond.html',eventss=result)
-
-# @app.route('/colgDetails/<string:id_no>', methods =['GET', 'POST'])
-# def colgDetails(id_no):
-# 	msg = ''
-# 	if request.method == 'POST' and 'id' in request.form and 'event_type' in request.form and 'event_name' in request.form and 'desc' in request.form and 'start_date' in request.form and 'end_date' in request.form and 'fee' in request.form:
-# 		# college = request.form['college']
-# 		# id = request.form['id']
-# 		event_type = request.form['event_type']
-# 		event_name = request.form['event_name']
-# 		# desc= request.form['desc']
-# 		start_date = request.form['start_date']
-# 		end_date=request.form['end_date']
-# 		fee= request.form['fee']
-# 		cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-# 		cursor.execute('SELECT * FROM eventss WHERE id = % s', (id_no, ))
-# 		event = cursor.fetchall()
-# 		# if event:
-# 		# 	msg = 'Event already exists !'
-# 		if not event_type or not event_name or not start_date or not end_date or not fee or not id :
-# 			msg = 'Please fill out the form !'
-# 		else:
-# 			cursor.execute('INSERT INTO eventss VALUES (% s, % s, % s, % s,% s)', ( event_type, event_name,start_date,end_date,fee, ))
-# 			mysql.connection.commit()
-# 			msg = 'Event added successfully !'
-# 			return render_template('index.html')
-# 	elif request.method == 'POST':
-# 		msg = 'Please fill out the form !'
-# 	return render_template('colgDetails.html', msg = msg)
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM eventss where event_type="Seminar"')
+    result = cursor.fetchall()
+    cursor.close()
+    return render_template('studSecond.html',eventss=result)
 
 
 @app.route('/index2')
 def index2():
-	cur = mysql.connection.cursor()
-	cur.execute("SELECT slno,event_name,event_type,start_date,end_date,fee FROM eventss where id=%s",(id,))
-	data = cur.fetchall()
-	cur.close()
-	return render_template('index2.html', eventss=data )
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT slno,event_name,event_type,start_date,end_date,fee FROM eventss where id=%s",(id,))
+    data = cur.fetchall()
+    cur.close()
+    return render_template('index2.html', eventss=data )
 
+# @app.route('/insert', methods = ['POST','GET'])
+# def insert():
+#     global slno
+#     if request.method == "POST":
+#         flash("Data Inserted Successfully")
+#         slno = request.form['slno']
+#         cur = mysql.connection.cursor()
+#         cur.execute("""
+#                UPDATE eventss
+#                SET slno=%s
+#                WHERE id=%s
+#             """, (slno,id))
+#         mysql.connection.commit()
+#         return redirect(url_for('insert2'))
 
-
-@app.route('/insert', methods = ['POST'])
+@app.route('/insert', methods = ['POST','GET'])
 def insert():
-
     if request.method == "POST":
         flash("Data Inserted Successfully")
-        # slno = request.form['slno']
+        slno = request.form['slno']
         event_name = request.form['event_name']
         event_type = request.form['event_type']
-        # desc = request.form['desc']
         start_date= request.form['start_date']
         end_date= request.form['end_date']
         fee= request.form['fee']
+        description= request.form['description']
         cur = mysql.connection.cursor()
-        cur.execute("""
-               UPDATE eventss
-               SET event_name=%s, event_type=%s, start_date=%s, end_date=%s, fee=%s
-               WHERE id=%s
-            """, (event_name, event_type,start_date,end_date,fee,id))
+        cur.execute("INSERT INTO eventss (id,slno,event_name, event_type, start_date,end_date,fee,description) VALUES (%s, %s,%s, %s, %s,%s,%s,%s)", (id,slno, event_name, event_type, start_date,end_date,fee,description))
         mysql.connection.commit()
         return redirect(url_for('index2'))
 
-
+# @app.route('/insert', methods = ['POST','GET'])
+# def insert():
+#     msg = ''
+#     if request.method == "POST":
+#         flash("Data Inserted Successfully")
+#         slno = request.form['slno']
+#         event_name = request.form['event_name']
+#         event_type = request.form['event_type']
+#         start_date= request.form['start_date']
+#         end_date= request.form['end_date']
+#         fee= request.form['fee']
+#         description= request.form['description']
+#         cur = mysql.connection.cursor()
+#         cur.execute("""
+#                UPDATE eventss
+#                SET slno=%s
+#                WHERE id=%s
+#             """, (slno,id))
+#         event=cur.fetchone()
+#         if event:
+#             msg= 'event exists'
+#         else:
+#             cur.execute("""UPDATE eventss
+#                SET event_name=%s, event_type=%s, start_date=%s, end_date=%s, fee=%s, description=%s WHERE slno=%s
+#             """, (event_name, event_type,start_date,end_date,fee,description,slno))
+#         mysql.connection.commit()
+#         return redirect(url_for('index2'))
 
 
 @app.route('/delete/<string:id_data>', methods = ['GET'])
@@ -217,36 +278,49 @@ def delete(id_data):
     return redirect(url_for('index2'))
 
 
-
-@app.route('/view', methods = ['POST','GET'])
-def view():
-	slno=request.form['slno']
-	cur = mysql.connection.cursor()
-	cur.execute("select desc FROM eventss where slno=%s",(slno))
-	mysql.connection.commit()
-	return redirect(url_for('index2'))
-
+@app.route('/view/<string:id_data>', methods = ['POST','GET'])
+def view(id_data):
+    cur = mysql.connection.cursor()
+    cur.execute("select description FROM eventss where slno=%s",(id_data))
+    mysql.connection.commit()
+    return redirect(url_for('description'))
 
 @app.route('/update',methods=['POST','GET'])
 def update():
-
     if request.method == 'POST':
         slno = request.form['slno']
         event_name = request.form['event_name']
         event_type = request.form['event_type']
-        desc = request.form['desc']
         start_date= request.form['start_date']
         end_date= request.form['end_date']
         fee= request.form['fee']
+        description = request.form['description']
         cur = mysql.connection.cursor()
         cur.execute("""
                UPDATE eventss
-               SET event_name=%s, event_type=%s, start_date=%s, end_date=%s, fee=%s, desc=%s
-               WHERE slno=%s
-            """, (event_name, event_type,start_date,end_date,fee,desc,slno))
+               SET event_name=%s, event_type=%s, start_date=%s, end_date=%s, fee=%s, description=%s WHERE slno=%s
+            """, (event_name, event_type,start_date,end_date,fee,description,slno))
         flash("Data Updated Successfully")
         mysql.connection.commit()
         return redirect(url_for('index2'))
+
+# app.route('/description')
+# def description():
+#     cur = mysql.connection.cursor()
+#     cur.execute("SELECT slno,event_name,event_type,start_date,end_date,fee FROM eventss where id=%s",(id,))
+#     data = cur.fetchall()
+#     cur.close()
+#     return render_template('description.html', eventss=data )
+
+@app.route('/regconfirm/<string:id_data>/<string:etype>', methods = ['GET'])
+def regconfirm(id_data,etype):
+    flash("Registered for event Successfully")
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM eventss WHERE slno=%s and event_type=%s", (id_data,etype))
+    data = cur.fetchall()
+    cur.close()
+    mysql.connection.commit()
+    return render_template('studSecond.html', eventss=data )
 
 
 import nltk
